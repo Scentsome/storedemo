b0fff28a4a39f9396aab3ea75c7200756852b4ac
Michael Pan <scentsome@gmail.com>
2017-06-14 17:02:09 +0800

    Fix: Load json

--- MyList/MyList/ViewController.swift
+++ MyList/MyList/ViewController.swift
@@ -8,27 +8,37 @@
 
 import UIKit
 import Alamofire
-
+struct Album {
+    var id:Int
+    var title:String
+    var userId:Int
+}
 class ViewController: UIViewController,UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
-    var list:[String] = ["Michael","James", "Allen"]
+    var list:[[String:Any]]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let urlPath = "https://jsonplaceholder.typicode.com/albums"
         Alamofire.request(urlPath).responseJSON { response in
             if let json = response.result.value {
                 print("JSON: \(json)")
+                self.list = json as? [[String:Any]]
+                self.tableView.reloadData()
             }
         }
         
         tableView.dataSource = self
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list.count
+        if list == nil {
+            return 0
+        }
+        return list!.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell:UITableViewCell? = tableView.dequeueReusableCell(withIdentifier: "ListCell")
-        cell?.textLabel?.text = list[indexPath.row]
+        cell?.textLabel?.text = list?[indexPath.row]["title"] as? String
         return cell!
     }
 
